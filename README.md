0c6db8a3dea0c5813a6c38a8864f16cffb011684
Joshua Gort <accounts@joshuagort.com>
2025-04-18 22:29:09 +0200

    Move build-package outside of chipcie-kotlin, update README

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ This is a Debian repository for packages used during programming contests organi
 2. Set up a build script which creates a .deb file, and places it in ``debian`` folder.
 3. Run [packages script](/debian/create-packages.sh) so it is included in the list of packages
 
+### How to use repository
+Add [chipcie-repo.list](/debian/chipcie-repo.list) to your apt sources at ``/etc/apt/sources.list.d/``. Don't forget to ``apt update`` after adding the repository.
 
 ## Resources
-https://assafmo.github.io/2019/05/02/ppa-repo-hosted-on-github.html
-https://askubuntu.com/a/493577
+- https://assafmo.github.io/2019/05/02/ppa-repo-hosted-on-github.html
+- https://askubuntu.com/a/493577
--- package-chipcie-kotlin.sh
+++ package-chipcie-kotlin.sh
@@ -1,38 +1,40 @@
 #!/usr/bin/env bash
 
+ecoh "Packaging chipcie-kotlin (note that version upgrades must occur in this script)"
+
 TMPFILE=$(mktemp)
 
 # Kotlin compiler zip file url
 KOTLIN_ZIP_URL="https://github.com/JetBrains/kotlin/releases/download/v2.1.20/kotlin-compiler-2.1.20.zip"
 
 # Ensure usr/local/lib directory exists and is empty
-mkdir -p "./usr/local/lib"
-if [ "$(ls -A ./usr/local/lib)" ]; then
-    echo "lib folder not empty, do you want to delete content? (y/n)"
+mkdir -p "./chipcie-kotlin/usr/local/lib"
+if [ "$(ls -A ./chipcie-kotlin/usr/local/lib)" ]; then
+    echo "Chipcie-kotlin lib folder not empty, do you want to delete content? (y/n)"
     read -r answer
     if [[ ! "$answer" =~ ^[Yy]$ ]]; then
         echo "Exiting..."
         exit 1
     fi
-    rm -rf ./usr/local/lib/*
+    rm -rf ./chipcie-kotlin/usr/local/lib/*
 fi
 
 wget -O "$TMPFILE" "$KOTLIN_ZIP_URL"
 
-unzip "$TMPFILE" -d "./usr/local/lib"
+unzip "$TMPFILE" -d "./chipcie-kotlin/usr/local/lib"
 
 # Update DEBIAN/control file
 
 ## Read Version from control file
-VERSION=$(grep -m 1 '^Version:' ./DEBIAN/control | cut -d ' ' -f 2)
+VERSION=$(grep -m 1 '^Version:' ./chipcie-kotlin/DEBIAN/control | cut -d ' ' -f 2)
 ## Ask for new version
 echo "Current version is $VERSION. Do you want to change it? (y/n)"
 read -r answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
     echo "Enter new version:"
     read -r new_version
-    sed -i "s/^Version: .*/Version: $new_version/" ./DEBIAN/control
+    sed -i "s/^Version: .*/Version: $new_version/" ./chipcie-kotlin/DEBIAN/control
 fi
 
 
-dpkg-deb --build . ../debian
+dpkg-deb --build ./chipcie-kotlin ./debian
